befe7544b02dc612ccef45f163ae003287009f6e
Lui Liu <liulewie@gmail.com>
2016-04-10 17:07:33 -0700

    Updated nested queries to unnested queries

--- queries.sql
+++ queries.sql
@@ -1,13 +1,11 @@
 -- Query 1
 -- Names of all the actors in the movie "Die Another Day"
 -- format: <firstname> <lastname>
-SELECT CONCAT(A.first, ' ', A.last) AS "Actors in 'Die Another Day'"
-FROM Actor AS A
-WHERE A.id = ANY (SELECT MA.aid
-                  FROM MovieActor AS MA
-                  WHERE MA.mid = (SELECT M.id
-                                  FROM Movie AS M
-                                  WHERE M.title = "Die Another Day"));
+SELECT CONCAT(a.first,' ',a.last) AS "Actors in 'Die Another Day'"
+FROM Movie AS m, MovieActor AS ma, Actor AS a
+WHERE m.id = ma.mid
+AND a.id = ma.aid
+AND title = 'Die Another Day';
 
 -- Query 2
 -- Count of all actors who acted in multiple movies
@@ -20,22 +18,19 @@ WHERE num_acted.num_movies >= 2;
 
 -- Query 3
 -- Title of movies that sell more than 1,000,000 tickets
-SELECT M.title AS "Movies that sold more than 1,000,000 tickets"
-FROM Movie AS M
-WHERE M.id = ANY (SELECT S.mid
-                  FROM Sales AS S
-                  WHERE S.ticketsSold > 1000000);
+SELECT "Movies that sold more than 1,000,000 tickets"
+FROM Movie AS m, Sales AS s
+WHERE m.id = s.mid
+AND ticketsSold > 1000000;
 
 -- Query 4
 -- Directors of movies that have an IMDB and Rotten Tomato rating at least 90
-SELECT CONCAT(D.last, ', ', D.first) AS "Directors of movies with IMDB and Rotten Tomatoes rating >= 90"
-FROM Director AS D
-WHERE D.id = ANY (SELECT DISTINCT MD.did
-                  FROM MovieDirector AS MD
-                  WHERE MD.mid = ANY (SELECT MR.mid
-                  	                  FROM MovieRating as MR
-                  	                  WHERE MR.imdb >= 90
-                  	                  AND MR.rot >= 90));
+SELECT CONCAT (d.first, ' ', d.last) As "Directors of movies with IMDB and Rotten Tomatoes rating >= 90"
+FROM MovieRating AS mr, MovieDirector AS md, Director AS d
+WHERE mr.mid = md.mid
+AND md.did = d.id
+AND mr.imdb >= 90
+AND mr.rot >= 90;
 
 -- Query 5
 -- Average total income per movies genre, and number of movies in each genre (for additional context)
