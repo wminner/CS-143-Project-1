13f5403d8c02673ab3e49326c59ae6af026c848d
Lui Liu <liulewie@gmail.com>
2016-04-14 17:31:43 -0700

    Added constraints. Updated create.sql and violate.sql

--- create.sql
+++ create.sql
@@ -1,6 +1,7 @@
--- Not sure about NULLs
+
+-- Primary Key Constraint: The movie ID (mid) needs to be the primary key as each movie should only have one entry
 CREATE TABLE Movie (
-	id INT NOT NULL AUTO_INCREMENT,
+	id INT NOT NULL,
 	title VARCHAR(100) NOT NULL,
 	year INT,
 	rating VARCHAR(10),
@@ -8,18 +9,20 @@ CREATE TABLE Movie (
 	PRIMARY KEY (id)
 );
 
+-- Primary Key Constraint: The actor ID (id) needs to be the primary key as each actor should only have one entry
+-- Check Constraint: The date of birth should either be null or preceed Date of Death
 CREATE TABLE Actor (
-	id INT NOT NULL AUTO_INCREMENT,
+	id INT NOT NULL,
 	last VARCHAR(20) NOT NULL,
 	first VARCHAR(20) NOT NULL,
 	sex VARCHAR(6),
 	dob DATE NOT NULL,
 	dod DATE,
-	PRIMARY KEY (id)
+	PRIMARY KEY (id),
+	CHECK(dod IS NULL OR dob < dod)
 );
 
--- Not sure about primary key or NULLs
--- mid itself should be enough since there is no other column in the table that can differetiate two tuples with different income/tickets for a given movie
+-- Referential Integrity Constraint: Sales.mid references Movie.id since the sales record is only relevent with respect to an existing movie
 CREATE TABLE Sales (
 	mid INT NOT NULL,
 	ticketsSold INT NOT NULL,
@@ -28,16 +31,19 @@ CREATE TABLE Sales (
 	FOREIGN KEY (mid) REFERENCES Movie(id) ON DELETE CASCADE
 )	ENGINE = InnoDB;
 
-
+-- Primary Key Constraint: The director ID (id) needs to be the primary key as each director should only have one entry
+-- Check Constraint: The Date of Birth should either be null or preceed 
 CREATE TABLE Director (
-	id INT NOT NULL AUTO_INCREMENT,
+	id INT NOT NULL,
 	last VARCHAR(20) NOT NULL,
 	first VARCHAR(20) NOT NULL,
 	dob DATE NOT NULL, 
 	dod DATE,
-	PRIMARY KEY (id)
+	PRIMARY KEY (id),
+	CHECK(dod IS NULL OR dob < dod)
 );
 
+-- Referential Integrity Constraint: MovieGenre.mid references Movie.id since genre is only applicable to an existing movie
 CREATE TABLE MovieGenre (
 	mid INT NOT NULL,
 	genre VARCHAR(20) NOT NULL,
@@ -45,6 +51,8 @@ CREATE TABLE MovieGenre (
 	FOREIGN KEY (mid) REFERENCES Movie(id) ON DELETE CASCADE
 )	ENGINE = InnoDB;
 
+-- Referential Integrity Constraint: MovieDirector.mid references Movie.id since movie director association is only applicable to an existing movie
+-- Referential Integrity Constraint: MovieDirector.did references Director.id since movie director association is only applicable to a director that already exist
 CREATE TABLE MovieDirector (
 	mid INT NOT NULL,
 	did INT NOT NULL,
@@ -53,6 +61,8 @@ CREATE TABLE MovieDirector (
 	FOREIGN KEY (did) REFERENCES Director(id) ON DELETE CASCADE
 )	ENGINE = InnoDB;
 
+-- Referential Integrity Constraint: MovieActor.mid references Movie.id since movie actor association is only applicable to an existing movie
+-- Referential Integrity Constraint: MovieActor.aid references Actor.id since movie actor association is only applicable to an existing actor
 CREATE TABLE MovieActor (
 	mid INT NOT NULL,
 	aid INT NOT NULL,
@@ -62,18 +72,22 @@ CREATE TABLE MovieActor (
 	FOREIGN KEY (aid) REFERENCES Actor(id) ON DELETE CASCADE
 )	ENGINE = InnoDB;
 
--- Not sure about primary key or NULLs
--- Either of imdb or rot can be missing for a giving movie rating. We may be able to ensure constaints during data ingest
+
+-- Check Constraint: Either imdb or rot should exist
+-- Check Constraint: IMDB Rating (imdb) should be either Null or between 0 and 100
+-- Check Constraint: Rotten Tomato Rating (rot) should be either Null or between 0 and 100
 CREATE TABLE MovieRating (
 	mid INT NOT NULL,
 	imdb INT,
 	rot INT,
 	PRIMARY KEY (mid),
-	FOREIGN KEY (mid) REFERENCES Movie(id) ON DELETE CASCADE
+	FOREIGN KEY (mid) REFERENCES Movie(id) ON DELETE CASCADE,
+	CHECK(imdb IS NOT NULL OR rot IS NOT NULL),
+	CHECK(imdb IS NULL OR (imdb <= 100 AND imdb >= 0)),
+	CHECK(rot IS NULL OR (rot <= 100 AND rot >= 0))
 )	ENGINE = InnoDB;
 
--- Not sure about primary key or NULLs
--- I assume a user can post review for a given movie multiple times.
+-- Check Constraint: Rating should be between 1 and 5
 CREATE TABLE Review (
 	name VARCHAR(20) NOT NULL,
 	time TIMESTAMP NOT NULL, 
@@ -81,7 +95,8 @@ CREATE TABLE Review (
 	rating INT,
 	comment VARCHAR(500),
 	PRIMARY KEY (name, time, mid),
-	FOREIGN KEY (mid) REFERENCES Movie(id) ON DELETE CASCADE
+	FOREIGN KEY (mid) REFERENCES Movie(id) ON DELETE CASCADE,
+	CHECK(rating <= 5 AND rating >= 1)
 )	ENGINE = InnoDB;
 
 CREATE TABLE MaxPersonID (
---
+++ violate.sql
@@ -0,0 +1,74 @@
+-- Primary key constraints 
+-- 1. The movie ID (mid) needs to be the primary key as
+-- each movie should only have one entry. Movie id = 2 is an existing movie in the Movie table. 
+
+INSERT INTO Movie 
+	VALUES (2, 'INVALID', 2016, 'G','Sony');
+
+-- 2. The actor ID (id) needs to be the primary key as each actor should only
+-- have one entry. Actor id = 1 is an existing actor in Actor table. 
+
+INSERT INTO Actor
+	VALUES (1, 'Smith', 'Actor', 'M', '1969-10-16', NULL);
+
+-- 3. The director ID (id) needs to be the primary key as each director should
+-- only have one entry. Director id = 16 is an existing director in the Director table.
+
+INSERT INTO Director
+	VALUES (16, 'Smith', 'Director', '1969-10-16', NULL);
+
+-- Referential integrity constraints 
+-- 1. Sales.mid references Movie.id since the
+-- sales record is only relevent with respect to an existing movie
+
+INSERT INTO Sales
+	VALUES ((SELECT MAX(id) +1 FROM Movie), 100, 100);
+
+-- 2. MovieGenre.mid references Movie.id since genre is only applicable to an
+-- existing movie
+
+INSERT INTO MovieGenre
+	VALUES ((SELECT MAX(id) +1 FROM Movie), 'Horror');
+
+-- 3. MovieDirector.mid references Movie.id since movie director association is
+-- only applicable to an existing movie. MAX(Movie.id) + 1 doens't exist. 
+
+INSERT INTO MovieDirector
+	VALUES ((SELECT MAX(id) +1 FROM Movie), (SELECT MAX(id) FROM Director));
+
+-- 4. MovieDirector.did references Director.id since movie director association
+-- is only applicable to a director that already exist. MAX(Director.id) + 1
+-- doesn't exist.
+
+INSERT INTO MovieDirector
+	VALUES ((SELECT MAX(id) FROM Movie), (SELECT MAX(id) +1 FROM Director));
+
+-- 5. MovieActor.mid references Movie.id since movie actor association is only
+-- applicable to an existing movie
+
+INSERT INTO MovieActor
+	VALUES((SELECT MAX(id) +1 FROM Movie), (SELECT MAX(id) FROM Actor), 'Lead');
+
+-- 6. MovieActor.aid references Actor.id since movie actor association is only
+-- applicable to an existing actor
+
+INSERT INTO MovieActor
+	VALUES((SELECT MAX(id) FROM Movie), (SELECT MAX(id) +1 FROM Actor), 'Lead');
+
+-- Check constraints
+-- 1. IMDB Rating (imdb) should be between 0 and 100
+
+UPDATE MovieRating 
+	SET imdb = 101 
+	WHERE mid = 1000;
+
+-- 2. RottenMTomato Rating (rot) should be between 0 and 100
+
+UPDATE MovieRating
+	SET rot = 101
+	WHERE mid = 1000;
+
+-- 3. Rating should be between 1 and 5
+
+INSERT INTO Review
+	VALUES('Lui', '2016-04-14', (SELECT MAX(id) FROM Movie), 6, 'Hello World');
